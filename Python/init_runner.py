f095794c0341e5f3897353f659b9f32027aa1196
James Hopbourn <jameshopbourn@gmail.com>
2022-10-28 23:15:23 +0800

    Python 脚本
    Signed-off-by: James Hopbourn <jameshopbourn@gmail.com>

---
+++ Python/init_runner.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+import os
+import pymysql
+import pandas as pd
+
+# 定义表格名字
+table = 'testM'
+
+# 连接数据库
+connection = pymysql.connect(
+	user='root',
+	password='newpassword',
+	host='127.0.01',
+	database='demo'
+)
+
+# 表格数据转为字典
+sheet = pd.read_excel('name.xlsx')
+data = sheet.to_dict(orient="records")
+
+# 清空表
+cursor = connection.cursor()
+cursor.execute(f"DELETE FROM {table}")
+connection.commit()
+
+# SQL 插入语句
+command = []
+for item in data:
+	command.append((f"{item['team_name'].rstrip()}", f"{item['personal_bib']:04}", f"{item['personal_name']}", f"{item['gender']}"))
+insert_query = f"INSERT INTO {table}(`team_name`,`personal_bib`,`personal_name`, `gender`) VALUES (%s, %s, %s, %s) "
+
+cursor.executemany(insert_query, command)
+connection.commit()
+cursor.close()
+connection.close()
---
+++ Python/update_time.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+import pymysql
+import pandas as pd
+
+# 定义表格名字
+table = 'testM'
+
+# 连接数据库
+connection = pymysql.connect(
+	user='root',
+	password='newpassword',
+	host='127.0.01',
+	database='demo'
+)
+
+# 表格数据转为字典
+sheet = pd.read_excel('data.xlsx')
+data = sheet.to_dict(orient="records")
+
+# SQL 插入语句
+command = []
+for item in data:
+	if(type(item['record_time']) is float): continue
+	command.append((f"{item['record_time']}", f"{item['personal_bib']:04}"))
+
+insert_query = f"update {table} set record_time= %s where personal_bib = %s "
+cursor = connection.cursor()
+cursor.executemany(insert_query, command)
+connection.commit()
+cursor.close()
+connection.close()
