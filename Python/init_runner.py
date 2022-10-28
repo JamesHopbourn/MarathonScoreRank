0bd83f8329aec398846357dda9a480881e9ca3b1
James Hopbourn <jameshopbourn@gmail.com>
2022-10-28 23:24:21 +0800

    Python 代码格式化
    Signed-off-by: James Hopbourn <jameshopbourn@gmail.com>

--- Python/init_runner.py
+++ Python/init_runner.py
@@ -9,10 +9,10 @@ table = 'testM'
 
 # 连接数据库
 connection = pymysql.connect(
-	user='root',
-	password='newpassword',
-	host='127.0.01',
-	database='demo'
+    user='root',
+    password='newpassword',
+    host='127.0.01',
+    database='demo'
 )
 
 # 表格数据转为字典
@@ -27,7 +27,8 @@ connection.commit()
 # SQL 插入语句
 command = []
 for item in data:
-	command.append((f"{item['team_name'].rstrip()}", f"{item['personal_bib']:04}", f"{item['personal_name']}", f"{item['gender']}"))
+    command.append((f"{item['team_name'].rstrip()}", f"{item['personal_bib']:04}",
+                   f"{item['personal_name']}", f"{item['gender']}"))
 insert_query = f"INSERT INTO {table}(`team_name`,`personal_bib`,`personal_name`, `gender`) VALUES (%s, %s, %s, %s) "
 
 cursor.executemany(insert_query, command)
--- Python/update_time.py
+++ Python/update_time.py
@@ -9,10 +9,10 @@ table = 'testM'
 
 # 连接数据库
 connection = pymysql.connect(
-	user='root',
-	password='newpassword',
-	host='127.0.01',
-	database='demo'
+    user='root',
+    password='newpassword',
+    host='127.0.01',
+    database='demo'
 )
 
 # 表格数据转为字典
@@ -22,8 +22,9 @@ data = sheet.to_dict(orient="records")
 # SQL 插入语句
 command = []
 for item in data:
-	if(type(item['record_time']) is float): continue
-	command.append((f"{item['record_time']}", f"{item['personal_bib']:04}"))
+    if (type(item['record_time']) is float):
+        continue
+    command.append((f"{item['record_time']}", f"{item['personal_bib']:04}"))
 
 insert_query = f"update {table} set record_time= %s where personal_bib = %s "
 cursor = connection.cursor()
