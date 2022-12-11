b4efd6b87ae88eab651c0435de8fc2f2d376b76d
James Hopbourn <jameshopbourn@gmail.com>
2022-12-11 21:22:03 +0800

    实体类生成注释
    Signed-off-by: James Hopbourn <jameshopbourn@gmail.com>

--- src/main/java/com/example/marathon/entity/Personal.java
+++ src/main/java/com/example/marathon/entity/Personal.java
@@ -20,63 +20,63 @@ public class Personal {
     /**
      * 参赛队名
      */
-    @Column(name = "team_name", nullable = false, columnDefinition = "char(20)")
+    @Column(name = "team_name", nullable = false, columnDefinition = "char(20) CHARACTER SET utf8mb4 COMMENT '参赛队名'")
     private String teamName;
 
     /**
      * 参赛号码
      * 此处使用 String 类型 避免开头的0被忽略 也方便后续的字母扩展
      */
-    @Column(name = "personal_bib", nullable = false, columnDefinition = "char(6)")
+    @Column(name = "personal_bib", nullable = false, columnDefinition = "char(6) CHARACTER SET utf8mb4 COMMENT '参赛号码'")
     private String personalBib;
 
     /**
      * 参赛选手名字
      */
-    @Column(name = "personal_name", nullable = false, columnDefinition = "char(4)")
+    @Column(name = "personal_name", nullable = false, columnDefinition = "char(4) CHARACTER SET utf8mb4 COMMENT '选手名字'")
     private String personalName;
 
     /**
      * 参赛选手性别
      */
-    @Column(name = "gender", nullable = false, columnDefinition = "char(1)")
+    @Column(name = "gender", nullable = false, columnDefinition = "char(1) CHARACTER SET utf8mb4 COMMENT '选手性别'")
     private String gender;
 
     /**
      * 计时时间
      */
-    @Column(name = "record_time", columnDefinition = "time")
+    @Column(name = "record_time", columnDefinition = "time COMMENT '计时时间'")
     private Time recordTime;
 
     /**
      * 性别排名
      */
-    @Column(name = "gender_rank", columnDefinition = "int")
+    @Column(name = "gender_rank", columnDefinition = "int(3) COMMENT '性别排名'")
     private Integer genderRank;
 
     /**
      * 全体排名
      */
-    @Column(name = "overall_rank", columnDefinition = "int")
+    @Column(name = "overall_rank", columnDefinition = "int(3) COMMENT '全体排名'")
     private Integer overallRank;
 
     /**
      * 视频排名
      * 用于个人计时赛近乎同时过线时通过视频区分前后
      */
-    @Column(name = "video_rank", columnDefinition = "int")
+    @Column(name = "video_rank", columnDefinition = "int(1) COMMENT '视频排名'")
     private Integer videoRank;
 
     /**
      * 实际用时
      */
-    @Column(name = "net_time", columnDefinition = "time")
+    @Column(name = "net_time", columnDefinition = "time COMMENT '平均配速'")
     private Time netTime;
 
     /**
-     * 配速
+     * 平均配速
      */
-    @Column(name = "avg_pace", columnDefinition = "char(6)")
+    @Column(name = "avg_pace", columnDefinition = "char(6) CHARACTER SET utf8mb4 COMMENT '平均配速'")
     private String avgPace;
 
     /**
